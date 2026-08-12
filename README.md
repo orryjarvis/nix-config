1167595004344a1288e99d81a2d8edcfddd67d7d
Orry Jarvis <orryjarvis@gmail.com>
2026-08-12 17:30:48 +0000

    initial flake for my personal nixos/wsl/home config

---
+++ README.md
@@ -0,0 +1,5 @@
+# TODO
+- build custom WSL tarball
+- file separation
+- ssh for github
+- git commit signature
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/release-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            nix.settings.experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+            system.stateVersion = "26.05";
+            wsl.enable = true;
+ 	    wsl.defaultUser = "orry";
+
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.orry = {
+              home.stateVersion = "26.05";
+
+              programs.git = {
+                enable = true;
+
+                settings = {
+                  user.name = "Orry Jarvis";
+                  user.email = "orryjarvis@gmail.com";
+                  pull.rebase = true;
+                  fetch.prune = true;
+                  push.autoSetupRemote = true;                  
+                  init.defaultBranch = "main";
+	        };
+              };
+            };
+          }
+        ];
+      };
+    };
+  };
+}
